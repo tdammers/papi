4498fb7864d219e6c63c4661fb95abab9a6a7718
Tobias Dammers <dammers@tracksinspector.com>
2016-10-13 17:08:26 +0200

    Removed dead code

--- example/app.py
+++ example/app.py
@@ -27,17 +27,3 @@ def application(env, start_response):
         'things': MyResource(things)
     }
     return serve_resources(resources, env, start_response)
-
-    # headers = [
-    #     ('Content-type', 'text/plain;charset=utf8')
-    # ]
-    # body = ''
-    # for k,v in sorted(dict(env).items()):
-    #     try:
-    #         v = json.dumps(v)
-    #     except TypeError:
-    #         v = repr(v)
-    #     body += "{0}: {1}\n".format(k, v)
-
-    # start_response('200 OK', headers)
-    # return body.encode('utf8')
