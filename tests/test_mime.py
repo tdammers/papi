33d4c01f56e259636cafac3d709c72478d10afef
Tobias Dammers <dammers@tracksinspector.com>
2016-10-18 15:47:12 +0200

    More unit tests

--- tests/test_mime.py
+++ tests/test_mime.py
@@ -1,6 +1,36 @@
 from papi.mime import *
 from tests.test_utils import assert_equal
 
+def test_parse_pair_happy():
+    src = "foo=bar"
+    expected = ("foo", "bar")
+    actual = parse_pair(src)
+    assert_equal(expected, actual)
+
+def test_parse_pair_degenerate():
+    src = "foo"
+    expected = ("foo", "")
+    actual = parse_pair(src)
+    assert_equal(expected, actual)
+
+def test_parse_pair_empty():
+    src = ""
+    expected = ("", "")
+    actual = parse_pair(src)
+    assert_equal(expected, actual)
+
+def test_get_q_happy():
+    t = MimeType("text", "plain", {"q": "0.2"})
+    expected = 0.2
+    actual = get_q(t)
+    assert_equal(expected, actual)
+
+def test_get_q_default():
+    t = MimeType("text", "plain", {})
+    expected = 1.0
+    actual = get_q(t)
+    assert_equal(expected, actual)
+
 def test_parse_mime_simple():
     src = "text/plain"
     expected = ("text", "plain", {})
@@ -41,7 +71,7 @@ def test_parse_http_accept():
     actual = parse_http_accept(src)
     assert_equal(expected, actual)
 
-def test_parse_http_accept():
+def test_parse_http_accept_sorted():
     src = "text/*;q=0.3,text/plain;q=0.6,*/*;q=0.1"
     expected = [
         ("text", "plain", {"q": "0.6"}),
@@ -79,6 +109,7 @@ def test_match_mime_positives():
 def test_match_mime_negatives():
     pairs = [
         ("text/*", "application/json", ()),
+        ("text/json", "text/plain", ()),
         ("text/plain;charset=latin1", "text/plain;charset=utf8", ("charset",)),
         ("text/plain;charset=utf8", "text/plain", ("charset", "q")),
     ]
@@ -86,3 +117,9 @@ def test_match_mime_negatives():
         pattern = parse_mime_type(pattern_str)
         candidate = parse_mime_type(candidate_str)
         assert(not match_mime(pattern, candidate, props))
+
+def test_mime_str_happy():
+    t = MimeType("text", "plain", {"q": "1.0"})
+    expected = "text/plain;q=1.0"
+    actual = mime_str(t)
+    assert_equal(expected, actual)
