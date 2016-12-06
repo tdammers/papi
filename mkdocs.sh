2de04b5a96286b5c3e302a454443b5c73a54f667
Tobias Dammers <dammers@tracksinspector.com>
2016-12-06 12:38:29 +0100

    Added zipfile creation to mkdocs script
    mkdocs now also generates a zipfile ready for upload to pythonhosted.

--- mkdocs.sh
+++ mkdocs.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+PACKAGE_VERSION=$(python setup.py --version)
 (
     cat docs-src/header.html
     pandoc --toc -f rst -t html README.rst
     cat docs-src/footer.html
 ) > docs/index.html
 cp docs-src/style.css docs/style.css
+cd docs
+zip "../dist/papi-$PACKAGE_VERSION-docs.zip" *
+cd ..
